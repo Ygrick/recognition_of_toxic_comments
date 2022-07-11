4854b89b5da056f8d22432d05f83665d05dcaf54
Y <igor2.004@mail.ru>
2022-07-11 20:18:21 +0300

    v1.5

--- src/BertClassifier.py
+++ src/BertClassifier.py
@@ -20,5 +20,18 @@ class BertClassifier(nn.Module):
         self.linear = nn.Linear(768, 2)
         self.relu = nn.ReLU()
 
+    def forward(self, input_id, mask):
+        """
+        Метод для прямого распространения ошибки (переопределенный)
+        :param input_id: входной номер
+        :param mask: маска
+        :return:
+        """
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask, return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
 
 
--- src/Dataset.py
+++ src/Dataset.py
@@ -1,6 +1,6 @@
 from transformers import BertTokenizer
 import torch
-
+import numpy as np
 
 class Dataset(torch.utils.data.Dataset):
     """
@@ -16,3 +16,44 @@ class Dataset(torch.utils.data.Dataset):
         self.texts = [tokenizer(text, padding='max_length', max_length = 512, truncation=True, return_tensors="pt")
                       for text in df['comment']
                       ]
+
+    def classes(self):
+        """
+        Метод получения всех строк класса датасета
+        :return: классы датасета списком
+        """
+        return self.labels
+
+    def __len__(self):
+        """
+        Метод получения длины датасета
+        :return: длина датасета
+        """
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        """
+        Метод получения опредленного количества строк классов
+        :param idx: набор индексов строк
+        :return: набор строк классов
+        """
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        """
+        Метод получения опредленного количества строк текста
+        :param idx: набор индексов строк
+        :return: набор строк текста
+        """
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+        """
+        Метод получения опредленного количества строк текста и классов
+        :param idx: набор индексов строк
+        :return: набор строк текста и классов
+        """
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
